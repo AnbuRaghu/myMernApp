e5daf3805d2bb57388aa7c22751d6d175eb828f0
AnbuRaghu <anbalagan28@yahoo.com>
2021-03-30 19:21:50 +0200

    publish to heroku

--- server/index.js
+++ server/index.js
@@ -2,25 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
-app.getMaxListeners('',(req,res)=>{
-  res.send('Hello to Memories API')
-})
+app.getMaxListeners("", (req, res) => {
+  res.send("Hello to Memories API");
+});
 
 // const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-//const CONNECTION_URL = "mongodb://localhost/memories";
+const CONNECTION_URL = "mongodb://localhost/memories";
 const PORT = process.env.PORT || 5000;
 
 mongoose
